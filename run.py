812262f617f9553915e31adf8ddb5f7a0245b153
Abdo Yasser <abdoyasser2018a@gmail.com>
2024-11-15 02:28:56 +0200

    a file to run the server

---
+++ run.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+from app.app import create_app
+from flask import jsonify
+
+app = create_app()
+
+@app.errorhandler(400)
+def bad_request_error(error):
+    return jsonify({
+        "status": "error: Bad Request",
+        "message": str(error.description)
+        }), 400
+
+@app.errorhandler(401)
+def unauthorized_error(error):
+    return jsonify({
+        "status": "error: Unauthorized",
+        "message": str(error.description)
+        }), 401
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return jsonify({
+        "status": "error: Not Found",
+        "message": str(error.description)
+        }), 404
+
+@app.errorhandler(409)
+def conflict_error(error):
+    return jsonify({
+        "status": "error: Conflict",
+        "message": str(error.description)
+        }), 409
+
+@app.errorhandler(429)
+def ratelimit_error(error):
+    return {
+        "status": "error: Too Many Requests",
+        "message": "You have exceeded the maximum number of requests allowed. Please try again later."
+        }, 429
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return jsonify({
+        "status": "error: Internal Server Error",
+        "message": str(error.description)
+        }), 500
+
+if __name__ == "__main__":
+    app.run(host="localhost", debug=True)
